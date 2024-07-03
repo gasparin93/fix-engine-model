405308ed19cd206d6522c6c944427e41a4617b05
Edgar Sias <eivansias@gmail.com>
2024-07-03 15:46:58 -0500

    Issue-7: finishing skeleton of header

--- src/main/java/com/honestefforts/fixengine/model/message/FixHeader.java
+++ src/main/java/com/honestefforts/fixengine/model/message/FixHeader.java
@@ -12,25 +12,46 @@ import lombok.Value;
 @Value
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class FixHeader implements FixObject<FixHeader> {
-
+public class FixHeader {
+  /**
+   * BeginString (tag 8), must be first element in the message
+   */
   @NonNull
-  String version;
+  String version; //should be position 1
   @NonNull
-  Integer checkSum;
+  Integer bodyLength; //should be position 2
   @NonNull
-  String messageType;
-  @NonNull
-  String senderCompID;
-  @NonNull
-  String targetCompID;
+  String messageType; //should be position 3
   @NonNull
   Integer msgSeqNum;
   @NonNull
   Instant sendingTime;
+  @NonNull
+  String senderCompID;
+  @NonNull
+  String targetCompID;
+  String onBehalfOfCompID;
+  String deliverToCompID;
+  Integer secureDataLength;
+  String secureData; //should be after secureDataLength, both must be included if one is
+  String senderSubID;
+  String senderLocationID;
+  String targetSubID;
+  String targetLocationID;
+  String onBehalfOfSubID;
+  String onBehalfOfLocationID;
+  String deliverToSubID;
+  String deliverToLocationID;
+  Boolean possibleDuplicationFlag; //Y/N
+  Boolean possibleResend; //Y/N
+  Instant originalSendingTime;
+  Integer xmlDataLength;
+  String xmlData;
+  String messageEncoding;
+  Integer lastMsgSeqNumProcessed;
+  Integer numberOfHops;
+  String hopCompID;
+  Instant hopSendingTime;
+  String hopRefID;
 
-  @Override
-  public FixHeader fromMapOfTags(@NonNull final Map<String, RawTag> mapOfTags) {
-    return FixHeader.builder().build();
-  }
 }
