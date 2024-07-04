566ad00a5831728c59d7e47ae19d02af5dec9cf3
Edgar Sias <eivansias@gmail.com>
2024-07-04 14:31:38 -0500

    Issue-7: adding fields to DiscretionInstructions component

--- src/main/java/com/honestefforts/fixengine/model/message/components/DiscretionInstructions.java
+++ src/main/java/com/honestefforts/fixengine/model/message/components/DiscretionInstructions.java
@@ -1,5 +1,19 @@
 package com.honestefforts.fixengine.model.message.components;
 
 public class DiscretionInstructions {
+  /** Tag 388 (DiscretionInst) */
+  Character discretionInstruction;
+  /** Tag 389 (DiscretionOffsetValue) */
+  Double discretionOffsetValue;
+  /** Tag 841 (DiscretionMoveType) */
+  Integer discretionMoveType;
+  /** Tag 842 (DiscretionOffsetType) */
+  Integer discretionOffsetType;
+  /** Tag 843 (DiscretionLimitType) */
+  Integer discretionLimitType;
+  /** Tag 844 (DiscretionRoundDirection) */
+  Integer discretionRoundDirection;
+  /** Tag 846 (DiscretionScope) */
+  Integer discretionScope;
 
 }
