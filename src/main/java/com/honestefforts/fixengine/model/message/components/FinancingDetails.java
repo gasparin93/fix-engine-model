f85989fb43e25ad1438b7a727d0d1f33c7498017
Edgar Sias <eivansias@gmail.com>
2024-07-04 14:39:04 -0500

    Issue-7: adding fields to FinancingDetails component

--- src/main/java/com/honestefforts/fixengine/model/message/components/FinancingDetails.java
+++ src/main/java/com/honestefforts/fixengine/model/message/components/FinancingDetails.java
@@ -1,5 +1,26 @@
 package com.honestefforts.fixengine.model.message.components;
 
+import com.honestefforts.fixengine.model.universal.Currency;
+import java.time.LocalDate;
+
 public class FinancingDetails {
+  /** Tag 913 (AgreementDesc) */
+  String agreementDesc;
+  /** Tag 914 (AgreementID) */
+  String agreementID;
+  /** Tag 915 (AgreementDate) */
+  LocalDate agreementDate;
+  /** Tag 918 (AgreementCurrency) */
+  Currency agreementCurrency;
+  /** Tag 788 (TerminationType) */
+  Integer terminationType;
+  /** Tag 916 (StartDate) */
+  LocalDate startDate;
+  /** Tag 917 (EndDate) */
+  LocalDate endDate;
+  /** Tag 919 (DeliveryType) */
+  Integer deliveryType;
+  /** Tag 898 (MarginRatio) */
+  Double marginRatio;
 
 }
