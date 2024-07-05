748725959e1d005b0c51cb6794cf9b7992d8aea2
Edgar Sias <eivansias@gmail.com>
2024-07-04 20:37:29 -0500

    Issue-7: adding component Stipulations

--- src/main/java/com/honestefforts/fixengine/model/message/components/Stipulations.java
+++ src/main/java/com/honestefforts/fixengine/model/message/components/Stipulations.java
@@ -1,5 +1,10 @@
 package com.honestefforts.fixengine.model.message.components;
 
 public class Stipulations {
-
+  /** Tag 232 (NoStipulations) */
+  Integer numberOfStipulations;
+  /** Tag 233 (StipulationType) */
+  String stipulationType;
+  /** Tag 234 (StipulationValue) */
+  String stipulationValue;
 }
