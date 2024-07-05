d5c559f3d7997f24c7de46bf80ac49ec319c61d8
Edgar Sias <eivansias@gmail.com>
2024-07-04 20:10:17 -0500

    Issue-7: adding component NestedPartes

--- src/main/java/com/honestefforts/fixengine/model/message/components/NestedParties.java
+++ src/main/java/com/honestefforts/fixengine/model/message/components/NestedParties.java
@@ -1,5 +1,18 @@
 package com.honestefforts.fixengine.model.message.components;
 
 public class NestedParties {
-
+  /** Tag 539 (NoNestedPartyIDs) */
+  Integer numberOfNestedPartyIds;
+  /** Tag 524 (NestedPartyID) */
+  String nestedPartyId;
+  /** Tag 525 (NestedPartyIDSource) */
+  Character nestedPartyIdSource;
+  /** Tag 538 (NestedPartyRole) */
+  Integer nestedPartyRole;
+  /** Tag 804 (NoNestedPartySubIDs) */
+  Integer numberOfNestedPartySubIds;
+  /** Tag 545 (NestedPartySubID) */
+  String nestedPartySubId;
+  /** Tag 805 (NestedPartySubIDType) */
+  Integer nestedPartySubIdType;
 }
