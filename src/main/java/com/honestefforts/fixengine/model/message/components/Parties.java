9e9b3cd001c44ed0f21548b1d378f777cddd9a7c
Edgar Sias <eivansias@gmail.com>
2024-07-04 20:20:07 -0500

    Issue-7: adding component Parties

--- src/main/java/com/honestefforts/fixengine/model/message/components/Parties.java
+++ src/main/java/com/honestefforts/fixengine/model/message/components/Parties.java
@@ -1,5 +1,18 @@
 package com.honestefforts.fixengine.model.message.components;
 
 public class Parties {
-
+  /** Tag 453 (NoPartyIDs) */
+  Integer numberOfPartyIds;
+  /** Tag 448 (PartyID) */
+  String partyId;
+  /** Tag 447 (PartyIDSource) */
+  Character partyIdSource;
+  /** Tag 452 (PartyRole) */
+  Integer partyRole;
+  /** Tag 802 (NoPartySubIDs) */
+  Integer numberOfPartySubIds;
+  /** Tag 523 (PartySubID) */
+  String partySubId;
+  /** Tag 803 (PartySubIDType) */
+  Integer partySubIdType;
 }
