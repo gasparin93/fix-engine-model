8e556b9e3221f64ff5523588ceae2fc0a2713e6a
Edgar Sias <eivansias@gmail.com>
2024-07-04 20:46:10 -0500

    Issue-7: adding component YieldData

--- src/main/java/com/honestefforts/fixengine/model/message/components/YieldData.java
+++ src/main/java/com/honestefforts/fixengine/model/message/components/YieldData.java
@@ -1,5 +1,18 @@
 package com.honestefforts.fixengine.model.message.components;
 
-public class YieldData {
+import java.time.LocalDate;
 
+public class YieldData {
+  /** Tag 235 (YieldType) */
+  String yieldType;
+  /** Tag 236 (Yield) */
+  Double yield;
+  /** Tag 701 (YieldCalcDate) */
+  LocalDate yieldCalcDate;
+  /** Tag 696 (YieldRedemptionDate) */
+  LocalDate yieldRedemptionDate;
+  /** Tag 697 (YieldRedemptionPrice) */
+  Double yieldRedemptionPrice;
+  /** Tag 698 (YieldRedemptionPriceType) */
+  Integer yieldRedemptionPriceType;
 }
