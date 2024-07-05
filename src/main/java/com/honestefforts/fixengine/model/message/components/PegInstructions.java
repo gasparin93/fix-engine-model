0ac18fb01c61ce199905a23db40844f6fe1e210a
Edgar Sias <eivansias@gmail.com>
2024-07-04 20:24:28 -0500

    Issue-7: adding component PegInstructions

--- src/main/java/com/honestefforts/fixengine/model/message/components/PegInstructions.java
+++ src/main/java/com/honestefforts/fixengine/model/message/components/PegInstructions.java
@@ -1,5 +1,16 @@
 package com.honestefforts.fixengine.model.message.components;
 
 public class PegInstructions {
-
+  /** Tag 211 (PegOffsetValue) */
+  Double pegOffsetValue;
+  /** Tag 835 (PegMoveType) */
+  Integer pegMoveType;
+  /** Tag 836 (PegOffsetType) */
+  Integer pegOffsetType;
+  /** Tag 837 (PegLimitType) */
+  Integer pegLimitType;
+  /** Tag 838 (PegRoundDirection) */
+  Integer pegRoundDirection;
+  /** Tag 840 (PegScope) */
+  Integer pegScope;
 }
