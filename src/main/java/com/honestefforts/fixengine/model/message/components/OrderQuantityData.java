74a4bb1807ada62fe7b55fcbb6dc29c4ac444fe0
Edgar Sias <eivansias@gmail.com>
2024-07-04 20:14:16 -0500

    Issue-7: adding component OrderQuantityData

--- src/main/java/com/honestefforts/fixengine/model/message/components/OrderQuantityData.java
+++ src/main/java/com/honestefforts/fixengine/model/message/components/OrderQuantityData.java
@@ -1,5 +1,14 @@
 package com.honestefforts.fixengine.model.message.components;
 
 public class OrderQuantityData {
-
+  /** Tag 38 (OrderQty) */
+  Integer orderQuantity;
+  /** Tag 152 (CashOrderQty) */
+  Integer cashOrderQuantity;
+  /** Tag 516 (OrderPercent) */
+  Double orderPercent;
+  /** Tag 468 (RoundingDirection) */
+  Character roundingDirection;
+  /** Tag 469 (RoundingModulus) */
+  Double roundingModulus;
 }
