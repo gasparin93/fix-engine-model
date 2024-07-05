9060580fa0c6617a360402a6b48b7f9af81e5083
Edgar Sias <eivansias@gmail.com>
2024-07-04 20:31:31 -0500

    Issue-7: adding component SpreadOrBenchmarkCurveData

--- src/main/java/com/honestefforts/fixengine/model/message/components/SpreadOrBenchmarkCurveData.java
+++ src/main/java/com/honestefforts/fixengine/model/message/components/SpreadOrBenchmarkCurveData.java
@@ -1,5 +1,22 @@
 package com.honestefforts.fixengine.model.message.components;
 
-public class SpreadOrBenchmarkCurveData {
+import com.honestefforts.fixengine.model.universal.Currency;
 
+public class SpreadOrBenchmarkCurveData {
+  /** Tag 218	(Spread) */
+  Double spread;
+  /** Tag 220 (BenchmarkCurveCurrency) */
+  Currency benchmarkCurveCurrency;
+  /** Tag 221 (BenchmarkCurveName) */
+  String benchmarkCurveName;
+  /** Tag 222 (BenchmarkCurvePoint) */
+  String benchmarkCurvePoint;
+  /** Tag 662 (BenchmarkPrice) */
+  Double benchmarkPrice;
+  /** Tag 663 (BenchmarkPriceType) */
+  Integer benchmarkPriceType;
+  /** Tag 699 (BenchmarkSecurityID) */
+  String benchmarkSecurityId;
+  /** Tag 761 (BenchmarkSecurityIDSource) */
+  String benchmarkSecurityIdSource;
 }
