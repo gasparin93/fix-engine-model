488033f5133200845e37340565a759fd4b701b66
Edgar Sias <eivansias@gmail.com>
2024-07-08 23:53:23 -0500

    Issue-10: making TagType position nullable, making validationError empty ignored in jacksonized, making response in response class nullable

--- src/main/java/com/honestefforts/fixengine/model/message/tags/RawTag.java
+++ src/main/java/com/honestefforts/fixengine/model/message/tags/RawTag.java
@@ -6,4 +6,5 @@ import lombok.NonNull;
 
 @Builder
 public record RawTag(@NonNull String tag, @NonNull String value, @NonNull TagType dataType,
-                     String version, int position) {}
+                     String version, Integer position) {
+}
